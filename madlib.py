22c79270f13bf9dd1bcca8d38367c3cc9859eafa
LaRenzo <dlarenzo@outlook.com>
2024-03-18 15:57:14 -0600

    Initial commit

---
+++ madlib.py
@@ -0,0 +1,5 @@
+noun = input("Please type a noun (like Fish, Dog, Cat): ")
+verb = input("Please type a verb (like action words run, dive, skip, fly): ")
+plural_noun = input("Please type a plural noun (like children, bikes, chairs, tables: ")
+
+print(f"The {noun} was the best warrior alive. They {verb} all the way to the moon and back home.  After they won the battle they had {plural_noun} come over and play Guitar Hero.  THE END")
